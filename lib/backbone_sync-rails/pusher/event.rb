d6db22cdf3c42d46d9ce05ded7cf4e911d314880
Graham Lyus <grahamlyus+github@gmail.com>
2012-04-06 12:37:11 -0700

    * Updates are also broadcast on a model specific channel. * Use trigger_async if EventMachine reactor is running. * Use a presence channel so subscriptions can be authorized. * RailsPusherSubscriber now takes a collection or model and bases the channel name on the object's url and only binds to updates if subscribing to a model.

--- lib/backbone_sync-rails/pusher/event.rb
+++ lib/backbone_sync-rails/pusher/event.rb
@@ -8,14 +8,26 @@ module BackboneSync
         end
 
         def broadcast
-          ::Pusher[channel].trigger! @event, data
+          trigger(collection_channel_name, @event, data)
+          trigger(model_channel_name, @event, data) if @event == :update
         end
 
         private
 
-        def channel
-          subchannel = @model.class.table_name
-          "sync-#{subchannel}"
+        def trigger(channel, event, data)
+          if defined?(EventMachine) && EventMachine.reactor_running?
+            ::Pusher[channel].trigger_async(event, data)
+          else
+            ::Pusher[channel].trigger!(event, data)
+          end
+        end
+
+        def collection_channel_name
+          "presence-sync-#{@model.class.table_name}"
+        end
+
+        def model_channel_name
+          "#{collection_channel_name}-#{@model.id}"
         end
 
         def data
